90b0c1e16cdeffeed41ac6bb0bc3a6248edc7d0d
Isstorya <isstorya@gmail.com>
2022-07-15 20:25:30 +0300

    add page n layout

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <MainHeader />
+    <SignUpComponent />
+    <LoginComponent />
+  </div>
+</template>
+
+<script>
+import Vue from "vue"
+import MainHeader from "../components/MainHeader.vue"
+import SignUpComponent from "../components/auth/SignUpComponent.vue"
+import LoginComponent from "../components/auth/LoginComponent.vue"
+
+export default {
+  data: () => ({}),
+  components: {
+    MainHeader,
+    SignUpComponent,
+    LoginComponent
+  }
+}
+
+</script>
